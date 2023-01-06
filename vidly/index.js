21493a36016e2bfcf4a9e6881d02ff3eb02575d5
Badass Nemesis <harshitanant29@gmail.com>
2023-01-06 19:08:54 +0530

    just used mongoose

--- vidly/index.js
+++ vidly/index.js
@@ -1,10 +1,18 @@
+const mongoose = require('mongoose');
 const Joi = require('Joi');
 const genres = require('./routes/genres')
+const customers = require('./routes/customers')
 const express = require('express');
 const app = express();
 
+mongoose.set('strictQuery', true); // this is only written to supress a warning in mongoose 7 upgradation
+mongoose.connect('mongodb://0.0.0.0:27017/mongo-exercises')
+    .then(() => console.log('Connected to MongoDB ....'))
+    .catch(err => console.error('Could not connect to MongoDB ....'));
+
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}......`));
--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,88 +1,83 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const Joi = require('Joi');
 
 router.use(express.json());
 
-const genres = [
-    { id: 1, name: 'Horror' },
-    { id: 2, name: 'Action' },
-    { id: 3, name: 'Boring' },
-];
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+    }
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
+// we could've written the whole new mongoose.schema instead of genreSchema here, but I like it that way
+
+router.get('/', async (req, res) => {
+    const genres = await Genre.find().sort('name');
+    res.send(genres);
+});
 
 validateGenre = (genre) => {
-    // making a schema object for letting Joi know, that the name property of the object that 
-    // we get, should be a string, minimum length should be 3, and it should be required
     const schema = Joi.object({
         name: Joi.string().min(3).required()
     });
 
-    // returning the boolean value of object by comapring with schema object
-    // return Joi.validate(genre, schema);
     return schema.validate(genre);
 }
 
-router.get('/', (req, res) => {
-    // just normal output of all the genres
-    res.send(genres);
-});
+router.get('/:id', async (req, res) => {
+    const genre = await Genre.findById(req.params.id);
 
-router.get('/:id', (req, res) => {
-    // this will store the whole genre's body of the given ID (if there is one)
-    const genre = genres.find(c => c.id == parseInt(req.params.id));
     if (genre == null) {
         return res.status(404).send('The genre with this ID was not found');
     }
 
-    // if found, just send the response
     res.send(genre);
 });
 
-router.post('/', (req, res) => {
-    const { error, value } = validateGenre(req.body);
-    if (error != null) {
+router.post('/', async (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) {
         return res.status(404).send(error.details[0].message);
     }
 
-    // making an object with the given res.body's name and just adding one more to id
-    const genre = {
-        id: genres.length + 1,
+    let genre = new Genre({
         name: req.body.name
-    }
+    });
+    genre = await genre.save();
 
-    // pushing the new object into genres list, and print the new object, bas aisehi.
-    genres.push(genre);
     res.send(genre);
 });
 
-router.put('/:id', (req, res) => {
-    // this will store the whole genre's body of the given ID (if there is one)
-    const genre = genres.find(c => c.id == parseInt(req.params.id));
-    if (genre == null) {
-        return res.status(404).send('The genre with this ID was not found');
-    }
-
+router.put('/:id', async (req, res) => {
     const { error } = validateGenre(req.body);
     if (error != null) {
         return res.status(400).send(error.details[0].message);
     }
 
-    // updating the name of the given genre's id by given name, and print it once
-    genre.name = req.body.name;
+    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+        new: true
+    });
+
+    if (!genre) {
+        return res.status(404).send('The genre with this ID was not found');
+    }
+
     res.send(genre);
 });
 
-router.delete('/:id', (req, res) => {
-    const genre = genres.find(c => c.id == parseInt(req.params.id));
+router.delete('/:id', async (req, res) => {
+    const genre = await Genre.findByIdAndRemove(req.params.id);
+
     if (genre == null) {
         return res.send(404).send('The genre with this ID was not found');
     }
 
-    // storing the index of where we found the ID, and using splice to remove from list
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-
-    // and just send the deleted item
     res.send(genre);
 });
 
